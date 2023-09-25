862376ebc6368a7b37d3ec78faed657d4c64d66f
Muhammad Ibrahim Kamal <139374245+mibrahim0499@users.noreply.github.com>
2023-09-25 22:18:35 +0500

    Accessed Event Error Handled

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,22 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { Container, Card } from 'react-bootstrap';
 import CustomNavbar from './CustomNavbar';
 import NotificationBox from './NotificationBox';
-import GalleryLink from './galleryUrl'; // Import the GalleryLink component
+//import GalleryLink from './galleryUrl'; // Import the GalleryLink component
 import axios from 'axios';
 import { useUserAuth } from "../UserContextProvider";
 
 function Dashboard() {
   const [events, setEvents] = useState([]);
+  const [accessedEvents, setAccessedEvents] = useState([]);
   const { user } = useUserAuth();
   const userId = user?.uid;
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API}/events/all/${userId}`)
       .then(response => {
+        console.log('Fetched main events:', response.data);
         setEvents(response.data);
+
+        // Fetch accessed events for each event
+        const accessedEventsPromises = response.data.map(event => 
+          axios.get(`${process.env.REACT_APP_API}/events/getgallery/${userId}/${event.id}`)
+        );
+
+        return Promise.all(accessedEventsPromises);
+      })
+      .then(accessedEventsResponses => {
+        console.log('Fetched accessed events responses:', accessedEventsResponses);
+        // Extract data from axios responses and set to state
+        const accessedEventsData = accessedEventsResponses.map(response => response.data);
+        console.log('Extracted accessed events data:', accessedEventsData);
+        setAccessedEvents(accessedEventsData);
       })
       .catch(error => {
-        console.error('Error fetching events: ', error);
+        console.error('Error fetching events or accessed events: ', error);
       });
   }, [userId]);
 
@@ -30,10 +46,9 @@ function Dashboard() {
           <h5>You are all checked in!</h5>
           <p>You'll get a text when photos from your event are uploaded.</p>
           <div style={{ display: 'grid' }}>
-            {events.map(event => (
+            {events.map((event, index) => (
               <div key={event.id}>
-                <NotificationBox event={event} />
-                <GalleryLink eventId={event.id} /> {/* Pass the eventId as a prop */}
+                <NotificationBox event={event} accessedEvent={accessedEvents[index]} />
               </div>
             ))}
           </div>
