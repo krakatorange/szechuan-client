97b56f6918844d371b1bb9a3eb26bb226d9f860d
Muhammad Ibrahim Kamal <139374245+mibrahim0499@users.noreply.github.com>
2023-09-29 21:24:49 +0500

    url polling

--- src/Components/DirectoryPoller.jsx
+++ src/Components/DirectoryPoller.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 
-function MonitorImages() {
+function MonitorImages({ eventId }) {
   const [url, setUrl] = useState("");
   const [images, setImages] = useState([]);
+  const [newImages, setNewImages] = useState([]);
+  const [uploadedImages, setUploadedImages] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const uploadInterval = useRef(null);
 
   const monitorImages = async () => {
     setLoading(true);
@@ -19,9 +24,10 @@ function MonitorImages() {
       );
 
       const data = response.data;
-      console.log("Received data:", data);
       if (data.image_urls && Array.isArray(data.image_urls)) {
-        setImages(data.image_urls);
+        const newDetectedImages = data.image_urls.filter(img => !images.includes(img) && !uploadedImages.includes(img));
+        setNewImages(prevImages => [...prevImages, ...newDetectedImages]);
+        setImages(prevImages => [...prevImages, ...newDetectedImages]);
       } else {
         console.error("Unexpected response format");
       }
@@ -32,8 +38,51 @@ function MonitorImages() {
     }
   };
 
-  const extractImageName = (url) => {
-    return url.split("/").pop().split("?")[0];
+  const startUploading = () => {
+    setIsUploading(true);
+
+    uploadInterval.current = setInterval(async () => {
+      for (const imageSrc of newImages) {
+        if (!uploadedImages.includes(imageSrc)) {
+          try {
+            const response = await axios.get(`${process.env.REACT_APP_API}/events/fetch-image`, {
+              params: {
+                imageUrl: imageSrc
+              },
+              responseType: 'arraybuffer'
+            });
+
+            const blob = new Blob([response.data], { type: 'image/jpeg' });
+
+            const formData = new FormData();
+            const imageName = imageSrc.split("/").pop().split("?")[0];
+            formData.append("galleryImage", blob, imageName);
+
+            await axios.post(
+              `${process.env.REACT_APP_API}/events/${eventId}/upload`,
+              formData,
+              {
+                headers: {
+                  "Content-Type": "multipart/form-data",
+                },
+              }
+            );
+
+            console.log(`Image ${imageName} uploaded successfully!`);
+
+            setUploadedImages(prevImages => [...prevImages, imageSrc]);
+            setNewImages(prevNewImages => prevNewImages.filter(img => img !== imageSrc));
+          } catch (error) {
+            console.error("Error uploading image:", error);
+          }
+        }
+      }
+    }, 5000); // Check every 5 seconds
+  };
+
+  const stopUploading = () => {
+    setIsUploading(false);
+    clearInterval(uploadInterval.current);
   };
 
   return (
@@ -48,13 +97,24 @@ function MonitorImages() {
         Monitor
       </button>
 
+      <button onClick={startUploading} disabled={isUploading}>
+        Start
+      </button>
+      <button onClick={stopUploading} disabled={!isUploading}>
+        Stop
+      </button>
+
       {loading && <p>Loading...</p>}
 
       <ul>
         {images.map((imageSrc, index) => (
           <li key={index} style={{ margin: "10px" }}>
-            <img src={imageSrc} alt={`Fetched Image ${index + 1}`} style={{ width: "100px" }} />
-            <p>{extractImageName(imageSrc)}</p>
+            <img
+              src={imageSrc}
+              alt={`Fetched Image ${index + 1}`}
+              style={{ width: "100px" }}
+            />
+            <p>{imageSrc.split("/").pop().split("?")[0]}</p>
           </li>
         ))}
       </ul>
