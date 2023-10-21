ddf3f508745a9a744cebaf40e76112334d6fc141
Ibrahim <m.ibrahim0499@gmail.com>
2023-10-21 21:41:39 +0500

    delete button successfully implemented

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Container, Card } from "react-bootstrap";
 import CustomNavbar from "./CustomNavbar";
 import NotificationBox from "./NotificationBox";
 import axios from "axios";
 import { useUserAuth } from "../UserContextProvider";
-import Logger from '../logger'; // make sure the path is correct
-
+import Logger from '../logger';
+import { io } from "socket.io-client";
 
 function Dashboard() {
   const [events, setEvents] = useState([]);
-  const [accessedEvents, setAccessedEvents] = useState([]); // Changed to a single object, not an array
-  const { user } = useUserAuth(); // Retrieve user from context
+  const [accessedEvents, setAccessedEvents] = useState([]);
+  const { user } = useUserAuth();
   const userId = user?.uid;
+  const socket = useRef(null);
 
-  useEffect(() => {
-    // Fetch the events created by the user
+  const fetchEvents = useCallback(() => {
     axios
       .get(`${process.env.REACT_APP_API}/events/all/${userId}`)
       .then((response) => {
@@ -22,20 +22,57 @@ function Dashboard() {
         setEvents(response.data);
       })
       .catch((error) => {
-        Logger.log("Error fetching main events: ");
+        Logger.error("Error fetching main events:", error);
       });
 
-    // Fetch the accessed event's details
     axios
       .get(`${process.env.REACT_APP_API}/events/getgallery/${userId}`)
       .then((response) => {
-        Logger.log("Fetched accessed event response:", response.data);
-        setAccessedEvents(response.data); // Set as an array of accessed events
+        Logger.log("Fetched accessed events:", response.data);
+        setAccessedEvents(response.data);
       })
       .catch((error) => {
-        Logger.log("Error fetching accessed event:");
+        Logger.error("Error fetching accessed events:", error);
       });
-  }, [userId]); // Dependency array with userId
+  }, [userId]);
+
+  useEffect(() => {
+    fetchEvents();
+  }, [fetchEvents]);
+
+  useEffect(() => {
+    socket.current = io(process.env.REACT_APP_API);
+
+    socket.current.on('event-created', (newEvent) => {
+      setEvents(prevEvents => [...prevEvents, newEvent]);
+    });
+
+    socket.current.on('event-deleted', (deletedEventId) => {
+      setEvents(prevEvents => prevEvents.filter(event => event.id !== deletedEventId));
+      setAccessedEvents(prevEvents => prevEvents.filter(event => event.id !== deletedEventId));
+    });
+
+    // More socket event listeners can be added here as needed
+
+    return () => {
+      socket.current.disconnect();
+    };
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  const deleteEvent = useCallback(
+    (eventId) => {
+      axios
+        .delete(`${process.env.REACT_APP_API}/events/event/${eventId}`)
+        .then((response) => {
+          Logger.log("Event deleted successfully:", response.data);
+          fetchEvents();  // refetch the events after deleting
+        })
+        .catch((error) => {
+          Logger.error("Error deleting event:", error);
+        });
+    },
+    [fetchEvents]
+  );
 
   return (
     <Container>
@@ -46,17 +83,11 @@ function Dashboard() {
           <h5>You are all checked in!</h5>
           <p>You'll get a text when photos from your event are uploaded.</p>
           <div style={{ display: "grid" }}>
-            {/* Display the events created by the user */}
-            {events.map((event, index) => (
-              <div key={event.id}>
-                <NotificationBox event={event} />
-              </div>
+            {events.map((event) => (
+              <NotificationBox key={event.id} event={event} onDelete={deleteEvent} />
             ))}
-            {/* Display the event the user has accessed, if available */}
-            {accessedEvents.map((accessedEvent, index) => (
-              <div key={accessedEvent.id}>
-                <NotificationBox event={accessedEvent} />
-              </div>
+            {accessedEvents.map((accessedEvent) => (
+              <NotificationBox key={accessedEvent.id} event={accessedEvent} onDelete={deleteEvent} />
             ))}
           </div>
         </Card.Body>
--- src/Components/NotificationBox.jsx
+++ src/Components/NotificationBox.jsx
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { parseISO, formatDistanceToNow, format } from 'date-fns';
 import { Link } from 'react-router-dom';
 import Logger from '../logger';
 
-function EventNotification({ event }) {
-  const { id, eventName, eventDateTime, coverPhotoUrl, eventLocation } = event; // Assuming you have eventLocation in your event object
+function EventNotification({ event, onDelete }) {
+  const { id, eventName, eventDateTime, coverPhotoUrl, eventLocation } = event;
   const eventTime = parseISO(eventDateTime);
   const [timeRemaining, setTimeRemaining] = useState('');
   const [alertShown, setAlertShown] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const deleteEvent = useCallback(() => {
+    // Call the onDelete prop with the event's ID
+    onDelete(id);
+  }, [onDelete, id]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -25,14 +35,25 @@ function EventNotification({ event }) {
     };
   }, [eventTime, eventName, alertShown]);
 
-  const formattedEventDateTime = format(eventTime, 'MMMM d, yyyy h:mm a'); // Format event date and time
+  const formattedEventDateTime = format(eventTime, 'MMMM d, yyyy h:mm a');
 
   return (
     <div className="col-lg-4 col-md-6 mb-4">
       <div className="card">
         <img src={coverPhotoUrl} className="card-img-top" alt={eventName} />
         <div className="card-body">
-          <h5 className="card-title">{eventName}</h5>
+          <h5 className="card-title">
+            {eventName}
+            <span className="float-right menu-button" onClick={toggleMenu}>
+              {/* Replace with your own three-dots icon */}
+              ☰
+            </span>
+            {menuVisible && (
+              <div className="card-menu">
+                <button onClick={deleteEvent}>Delete</button>
+              </div>
+            )}
+          </h5>
           <p className="card-text">Event Date/Time: {formattedEventDateTime}</p>
           <p className="card-text">Location: {eventLocation}</p>
           <p className="card-text">Time remaining: {timeRemaining}</p>
