5b7a8710ef8ccd848cd77dfa1c3f7d5e1ccd0ebb
Ibrahim <m.ibrahim0499@gmail.com>
2023-11-02 18:49:56 +0500

    Edit button implemented

--- src/Components/NotificationBox.jsx
+++ src/Components/NotificationBox.jsx
@@ -2,23 +2,61 @@ import React, { useState, useEffect, useCallback } from "react";
 import { parseISO, formatDistanceToNow, format } from "date-fns";
 import { Link } from "react-router-dom";
 import Logger from "../logger";
-import './css/notificationBox.css';
+import "./css/notificationBox.css";
+import EditEvent from "./UpdateEvent";
+import { Modal, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-function EventNotification({ event, onDelete }) {
+function EventNotification({ event, onDelete, onEventUpdated }) {
   const { id, eventName, eventDateTime, coverPhotoUrl, eventLocation } = event;
   const eventTime = parseISO(eventDateTime);
   const [timeRemaining, setTimeRemaining] = useState("");
   const [alertShown, setAlertShown] = useState(false);
   const [menuVisible, setMenuVisible] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
 
+  const handleEdit = () => {
+    setShowEditModal(true);
+    toggleMenu();
+  };
+
+  const handleCloseEditModal = () => {
+    setShowEditModal(false);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (!e.target.closest(".menu-button") && !e.target.closest(".card-menu")) {
+      setMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (menuVisible) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [menuVisible]);
+
   const deleteEvent = useCallback(() => {
     // Call the onDelete prop with the event's ID
     onDelete(id);
   }, [onDelete, id]);
+
+  const handleEventUpdated = (updatedEvent) => {
+    onEventUpdated(updatedEvent); // Call the function passed from Dashboard
+    setShowEditModal(false); // Close the modal
+    navigate("/dashboard"); // Navigate to the dashboard
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -45,19 +83,47 @@ function EventNotification({ event, onDelete }) {
           <img src={coverPhotoUrl} alt={eventName} />
           <div className="event-details-overlay">
             <h5 className="card-title">{eventName}</h5>
-            <p className="card-text">Event Date/Time: {formattedEventDateTime}</p>
+            <p className="card-text">
+              Event Date/Time: {formattedEventDateTime}
+            </p>
             <p className="card-text">Location: {eventLocation}</p>
             <p className="card-text">Time remaining: {timeRemaining}</p>
           </div>
-          <span className="menu-button" onClick={toggleMenu}>...</span>
+          <span className="menu-button" onClick={toggleMenu}>
+            ...
+          </span>
           {menuVisible && (
             <div className="card-menu">
-              <button className="delete-button" onClick={deleteEvent}>Delete</button>
+              <button className="edit-button" onClick={handleEdit}>
+                Edit
+              </button>
+              <button className="delete-button" onClick={deleteEvent}>
+                Delete
+              </button>
             </div>
           )}
         </div>
-        <Link to={`/event/${id}`} className="go-gallery-button">Go to Gallery</Link>
+        <Link to={`/event/${id}`} className="go-gallery-button">
+          Go to Gallery
+        </Link>
       </div>
+      {/* Edit Event Modal */}
+      <Modal
+        show={showEditModal}
+        onHide={handleCloseEditModal}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <EditEvent
+            eventId={event.id} // pass the event ID to EditEvent
+            onEventUpdated={handleEventUpdated} // Pass the local function that handles event update
+          />
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
---
+++ src/Components/UpdateEvent.jsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { useUserAuth } from "../UserContextProvider";
+import Logger from "../logger";
+import { io } from "socket.io-client";
+
+const socket = io(process.env.REACT_APP_API);
+
+function EditEvent({eventId, onEventUpdated}) {
+  const [currentEvent, setCurrentEvent] = useState(null);
+  const [eventName, setEventName] = useState("");
+  const [eventDateTime, setEventDateTime] = useState("");
+  const [eventLocation, setEventLocation] = useState("");
+  const [coverPhoto, setCoverPhoto] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useUserAuth();
+  const navigate = useNavigate();
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const formData = new FormData();
+      formData.append("eventName", eventName);
+      formData.append("eventDateTime", eventDateTime);
+      formData.append("eventLocation", eventLocation);
+      if (coverPhoto) {
+        formData.append("coverPhoto", coverPhoto);
+      }
+
+      const response = await axios.patch(
+        `${process.env.REACT_APP_API}/events/${eventId}/edit`,
+        formData
+      );
+      Logger.log("Event updated successfully:", response.data);
+
+      // Emit an event-updated event to the server
+      socket.emit("event-updated", response.data);
+
+      // If you want to navigate back to the dashboard after the update
+      // navigate("/dashboard");
+      onEventUpdated(response.data);
+    } catch (error) {
+      Logger.error("Error updating event: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleExit = () => {
+    navigate("/dashboard");
+  };
+
+  const handleCoverPhotoChange = (e) => {
+    setCoverPhoto(e.target.files[0]);
+  };
+
+  const containerStyle = {
+    maxWidth: "90%",
+    padding: "0 15px",
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Container style={containerStyle}>
+      <div className="d-flex justify-content-center align-items-center form-container">
+        <div className="vertical-form">
+          <h2>Edit Event Information</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="eventName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                value={eventName}
+                onChange={(e) => setEventName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="eventDateTime">
+              <Form.Label>Date/Time</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                value={eventDateTime}
+                onChange={(e) => setEventDateTime(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="eventLocation">
+              <Form.Label>Location</Form.Label>
+              <Form.Control
+                type="text"
+                value={eventLocation}
+                onChange={(e) => setEventLocation(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="coverPhoto">
+              <Form.Label>Cover Photo</Form.Label>
+              <div className="d-flex align-items-center">
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="coverPhoto"
+                  onChange={(e) => setCoverPhoto(e.target.files[0])}
+                />
+                {coverPhoto && (
+                  <img
+                    src={URL.createObjectURL(coverPhoto)}
+                    alt="Cover"
+                    className="ml-3"
+                    style={{ maxWidth: "100px", maxHeight: "100px" }}
+                  />
+                )}
+              </div>
+            </Form.Group>
+            <Button type="submit" variant="primary" className="mt-3">
+              Update Event
+            </Button>
+            <Button
+              variant="secondary"
+              className="mt-3 ml-2"
+              onClick={handleExit}
+            >
+              Cancel
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default EditEvent;
